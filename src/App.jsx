6fcf7a21a376fb98eed6be863742c56f4bfc62f8
SandipDeb <sandipdeb05@gmail.com>
2024-07-10 22:47:50 +0530

    basic quick start

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,15 @@
+import { useQuery } from "@tanstack/react-query";
 import "./App.css";
+import { fetchPosts } from "./api/api";
 
 function App() {
+  const { data, isLoading, isError, error, status } = useQuery({
+    queryKey: ["posts"],
+    queryFn: fetchPosts,
+  });
+
+  console.log(isLoading, status, data);
+
   return (
     <div className="container">
       <h1>Lets Build a Better 2025 ❤</h1>
