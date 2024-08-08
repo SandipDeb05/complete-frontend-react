8c0ab1deaa3bcf709587b5de1e6585d906d5bcb7
SandipDeb <sandipdeb05@gmail.com>
2024-08-08 18:13:19 +0530

    fix:(app) border spacing added

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,9 +1,10 @@
 import AddTodo from "../features/todo/AddTodo";
 import Todos from "../features/todo/Todos";
+import "../App.css";
 
 function App() {
   return (
-    <div className="container">
+    <div className="container border m-3 p-3 m-2">
       <h1>Lets Build a Better 2025 ❤</h1>
       <AddTodo />
       <Todos />
